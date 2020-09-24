90ca67178c7490b8b7cacb8791491325649f833f
Emily Aquin <eaquin1@gmail.com>
2020-09-24 14:47:53 -0500

    changed redis variable

--- config/redis-config.js
+++ config/redis-config.js
@@ -1,8 +1,7 @@
-const redis = require('redis');
+const redis = require("redis");
 
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_PORT = process.env.REDIS_URL || 6379;
 
 const redis_client = redis.createClient(REDIS_PORT);
 
 module.exports = redis_client;
-
