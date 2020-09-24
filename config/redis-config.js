9224eeb76628ac2f88e19142cc4b736795b3466d
Emily Aquin <eaquin1@gmail.com>
2020-09-24 15:02:04 -0500

    added redis console.log

--- config/redis-config.js
+++ config/redis-config.js
@@ -1,7 +1,7 @@
 const redis = require("redis");
 
 const REDIS_URL = process.env.REDIS_URL || 6379;
-
+console.log("REDIS_URL", REDIS_URL);
 const redis_client = redis.createClient(REDIS_URL);
 
 module.exports = redis_client;
