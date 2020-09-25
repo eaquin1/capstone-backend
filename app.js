7bb79a1b3fce4e7beb7b6375fb7eebc2721cc69d
Emily Aquin <eaquin1@gmail.com>
2020-09-25 14:23:21 -0500

    fixed typo in REDIS_URL

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(
         },
         store: new redisStore({
             //host: "localhost",
-            url: process.env.REDIS_ENV,
+            url: process.env.REDIS_URL,
             // port: 6480, // 6379,
             client: redisClient,
         }),
