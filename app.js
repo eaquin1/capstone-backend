be30b5c5e9bc988e1e661ecffb52b396cc77116e
Emily Aquin <eaquin1@gmail.com>
2020-09-25 14:20:18 -0500

    removed port from app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(
         store: new redisStore({
             //host: "localhost",
             url: process.env.REDIS_ENV,
-            port: 6480, // 6379,
+            // port: 6480, // 6379,
             client: redisClient,
         }),
         name: "dexcom_user",
