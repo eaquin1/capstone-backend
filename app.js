5b886c1411b0e644dadea47357b9cae2557a8b3e
Emily Aquin <eaquin1@gmail.com>
2020-09-26 12:17:36 -0500

    added app.set, trust proxy

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(shouldSendSameSiteNone);
-
+app.set("trust proxy", 1);
 redisClient.on("error", (err) => {
     console.log("Redis error: ", err);
 });
