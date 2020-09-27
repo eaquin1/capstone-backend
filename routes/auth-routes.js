5699b96f37c257aa6ad12cada93633fb550e4af0
Emily Aquin <eaquin1@gmail.com>
2020-09-27 11:15:55 -0500

    added req.session.save

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -32,7 +32,9 @@ router.get("/dexcom/redirect", passport.authenticate("oauth2"), (req, res) => {
     // }).
     //console.log("user", req.session.passport.user);
     // const token = createToken(req.session.passport.user);
-    return res.redirect(`${frontEnd}/home`);
+    req.session.save(() => {
+        res.redirect(`${frontEnd}/home`);
+    });
 });
 
 //route to check cookie against req.sessionID
