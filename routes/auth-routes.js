13e8152a2eb3d58f90cdfbf241dd9d954acbe827
Emily Aquin <eaquin1@gmail.com>
2020-09-27 17:34:03 -0500

    secure session

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -27,47 +27,42 @@ router.get(
 
 // callback route for dexcom to redirect to
 router.get("/dexcom/redirect", passport.authenticate("oauth2"), (req, res) => {
-    res.cookie("id", req.sessionID, {
-        expires: new Date(Date.now() + 7200 * 1000),
-        sameSite: "none",
-        secure: true,
-    }).redirect(`${frontEnd}/home`);
+    // res.cookie("id", req.sessionID, {
+    //     expires: new Date(Date.now() + 7200 * 1000),
+    //     sameSite: "none",
+    //     secure: true,
+    // }).redirect(`${frontEnd}/home`);
     //console.log("user", req.session.passport.user);
 
     // console.log("req.session inside redirect", req.session.passport);
-    // req.session.save(() => {
-    //     return res.redirect(`${frontEnd}/home`);
-    // });
+    //req.session.save(() => {
+    return res.redirect(`${frontEnd}/home`);
+    //);
 });
 
 //route to check cookie against req.sessionID
 router.get("/user", (req, res, next) => {
     console.log("req.session.passport", req.sessionID);
-    // try {
-    //     if (req.isAuthenticated()) {
-    //         const token = createToken(req.session.passport.user);
-    //         return res.json({ token });
-    //     } else {
-    //         return res.json(null);
-    //     }
-    //     // if (!req.session.passport) {
-    //     //     return res.json(null);
-    //     // } else if (req.session.passport.user) {
-    //     //     const token = createToken(req.session.passport.user);
-    //     //     return res.json({ token });
-    //     // }
-    // } catch (error) {
-    //     next(error);
-    // }
+    console.log("Authenticated?", req.isAuthenticated());
+    try {
+        if (req.isAuthenticated()) {
+            const token = createToken(req.session.passport.user);
+            return res.json({ token });
+        } else {
+            return res.json(null);
+        }
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.get("/logout", (req, res) => {
     req.logout();
-    req.session = null;
-    res.end();
-    // req.session.destroy((err) => {
-    //     res.send("Logged Out");
-    // });
+    // req.session = null;
+    // res.end();
+    req.session.destroy((err) => {
+        res.send("Logged Out");
+    });
 });
 
 module.exports = router;
